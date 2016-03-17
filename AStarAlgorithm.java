ce07d90f75bb61f5dc1f1de2eed46f7908b86c02
DarkAce65 <tvdood99@gmail.com>
2016-03-17 13:15:28 -0400

    Start creating AStarAlgorithm in Java

---
+++ AStarAlgorithm.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class AStarAlgorithm {
+	private ArrayList<Node> open;
+	private ArrayList<Node> closed;
+
+	public AStarAlgorithm() {
+		open = new ArrayList<Node>();
+		closed = new ArrayList<Node>();
+	}
+}
