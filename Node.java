40fa8fdf641e3007acefab7f779cba5e3cd14456
DarkAce65 <tvdood99@gmail.com>
2016-03-17 18:05:03 -0400

    Remove cost variable

--- Node.java
+++ Node.java
@@ -4,7 +4,6 @@ public class Node extends Point implements Comparable<Node> {
 	private final int type; // 0 - space, 1 - start, 2 - end, 3 - wall
 	private int stepCost = 0;
 	private int heuristicCost = 0;
-	private int cost = 0;
 
 	public Node(int type) {
 		super(0, 0);
@@ -28,17 +27,16 @@ public class Node extends Point implements Comparable<Node> {
 	}
 
 	public int getCost() {
-		return cost;
+		return stepCost + heuristicCost;
 	}
 
 	public void setCosts(int stepCost, int heuristicCost) {
 		this.stepCost = stepCost;
 		this.heuristicCost = heuristicCost;
-		cost = stepCost + heuristicCost;
 	}
 
 	public int compareTo(Node p) {
-		int diff = this.cost - p.getCost();
+		int diff = this.getCost() - p.getCost();
 		if(diff == 0) {
 			diff = this.stepCost - p.getStepCost();
 		}
@@ -46,6 +44,6 @@ public class Node extends Point implements Comparable<Node> {
 	}
 
 	public String toString() {
-		return "Type: " + this.type + ", Cost: " + this.stepCost + " + " + this.heuristicCost + " = " + this.cost;
+		return "Type: " + this.type + ", Cost: " + this.stepCost + " + " + this.heuristicCost + " = " + (this.stepCost + this.heuristicCost);
 	}
 }
