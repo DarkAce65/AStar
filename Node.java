b9bc5498dd576a512ca7bc1a24a306f45fcf948d
DarkAce65 <tvdood99@gmail.com>
2016-03-17 16:56:42 -0400

    Distinguish costs and add type variable

--- Node.java
+++ Node.java
@@ -1,21 +1,48 @@
 import java.awt.Point;
 
 public class Node extends Point implements Comparable<Node> {
+	private final int type; // 0 - space, 1 - start, 2 - end, 3 - wall
+	private int stepCost = 0;
+	private int heuristicCost = 0;
 	private int cost = 0;
 
-	public Node(int x, int y) {
+	public Node(int type, int x, int y) {
 		super(x, y);
+		this.type = type;
+	}
+
+	public Node(int x, int y) {
+		this(0, x, y);
+	}
+
+	public int getType() {
+		return type;
+	}
+
+	public int getStepCost() {
+		return stepCost;
+	}
+
+	public int getHeuristicCost() {
+		return heuristicCost;
 	}
 
 	public int getCost() {
 		return cost;
 	}
 
-	public void evaluateCost(int stepCost, Node end) {
-		cost = stepCost + Math.abs(end.getX() - this.getX()) + Math.abs(end.getY() - this.getY());
+	public int evaluateCost(int stepCost, Node end) {
+		this.stepCost = stepCost;
+		this.heuristicCost = Math.abs(end.x - this.x) + Math.abs(end.y - this.y);
+		cost = stepCost + heuristicCost;
+		return cost;
 	}
 
 	public int compareTo(Node p) {
-		return this.cost - p.getCost();
+		int diff = this.cost - p.getCost();
+		if(diff == 0) {
+			diff = this.stepCost - p.getStepCost();
+		}
+		return diff;
 	}
 }
