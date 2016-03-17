ccbcaeefa491c61501b788b13fd0fe685d15e674
DarkAce65 <tvdood99@gmail.com>
2016-03-17 16:10:56 -0400

    Begin restructuring AStarAlgorithm

--- AStarAlgorithm.java
+++ AStarAlgorithm.java
@@ -1,31 +1,19 @@
 import java.util.ArrayList;
 
 public class AStarAlgorithm {
-	private Node start;
-	private Node end;
+	private ArrayList<Node> steps;
 
 	private CostSortedNodeList<Node> open;
 	private CostSortedNodeList<Node> closed;
-	private ArrayList<Node> steps;
 
-	public AStarAlgorithm(int sx, int sy, int ex, int ey) {
-		start = new Node(sx, sy);
-		end = new Node(ex, ey);
+	public AStarAlgorithm() {
+		steps = new ArrayList<Node>();
 
 		open = new CostSortedNodeList<Node>();
 		closed = new CostSortedNodeList<Node>();
-		steps = new ArrayList<Node>();
 	}
 
-	public void setStart(int x, int y) {
-		start.setPosition(x, y);
-	}
-
-	public void setEnd(int x, int y) {
-		end.setPosition(x, y);
-	}
-
-	public void findPath() {
+	public void findPath(Node[][] map, Node start, Node end) {
 		open.empty();
 		closed.empty();
 		steps.empty();
