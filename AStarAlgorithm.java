6efc75c44a3af26e8ea7c07db9e9498b3f2ed700
DarkAce65 <tvdood99@gmail.com>
2016-03-17 13:32:56 -0400

    Add start and end nodes

--- AStarAlgorithm.java
+++ AStarAlgorithm.java
@@ -1,11 +1,16 @@
 import java.util.ArrayList;
 
 public class AStarAlgorithm {
+	private Node start;
+	private Node end;
 	private ArrayList<Node> open;
 	private ArrayList<Node> closed;
 
-	public AStarAlgorithm() {
+	public AStarAlgorithm(int sx, int sy, int ex, int ey) {
+		start = new Node(sx, sy);
+		end = new Node(ex, ey);
 		open = new ArrayList<Node>();
+		open.add(start);
 		closed = new ArrayList<Node>();
 	}
 }
