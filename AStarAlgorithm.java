df43e543c04a600156d17123e3323eda96c9402d
DarkAce65 <tvdood99@gmail.com>
2016-03-17 13:49:12 -0400

    Finish base variables and methods

--- AStarAlgorithm.java
+++ AStarAlgorithm.java
@@ -6,11 +6,21 @@ public class AStarAlgorithm {
 	private ArrayList<Node> open;
 	private ArrayList<Node> closed;
 
+	private ArrayList<Node> steps;
+
 	public AStarAlgorithm(int sx, int sy, int ex, int ey) {
 		start = new Node(sx, sy);
 		end = new Node(ex, ey);
 		open = new ArrayList<Node>();
-		open.add(start);
 		closed = new ArrayList<Node>();
+		steps = new ArrayList<Node>();
+	}
+
+	public void setStart(int x, int y) {
+		start.setPosition(x, y);
+	}
+
+	public void setEnd(int x, int y) {
+		end.setPosition(x, y);
 	}
 }
