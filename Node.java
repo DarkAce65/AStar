316e4bc2f7da05dfcf7fdb72b41daf6b015208fa
DarkAce65 <tvdood99@gmail.com>
2016-03-17 13:48:19 -0400

    Add methods for sorting

--- Node.java
+++ Node.java
@@ -1,9 +1,17 @@
 import java.awt.geom.Point2D;
 
-public class Node extends Point2D.Double {
+public class Node extends Point2D.Double implements Comparable<Node> {
 	private int cost = 0;
 
 	public Node(int x, int y) {
 		super(x, y);
+	}
+
+	public int getCost() {
+		return cost;
+	}
+
+	public int compareTo(Node p) {
+		return this.cost - p.getCost();
 	}
 }
