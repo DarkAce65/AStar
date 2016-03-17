9b1dc51417ad3a72d2f91d8bb4f9b256c9c4b39e
DarkAce65 <tvdood99@gmail.com>
2016-03-17 14:02:32 -0400

    Change Node to extend Point

--- Node.java
+++ Node.java
@@ -1,6 +1,6 @@
-import java.awt.geom.Point2D;
+import java.awt.Point;
 
-public class Node extends Point2D.Double implements Comparable<Node> {
+public class Node extends Point implements Comparable<Node> {
 	private int cost = 0;
 
 	public Node(int x, int y) {
@@ -9,6 +9,10 @@ public class Node extends Point2D.Double implements Comparable<Node> {
 
 	public int getCost() {
 		return cost;
+	}
+
+	public void evaluateCost(int stepCost, Node end) {
+		cost = stepCost + Math.abs(end.getX() - this.getX()) + Math.abs(end.getY() - this.getY());
 	}
 
 	public int compareTo(Node p) {
