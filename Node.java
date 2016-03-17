b901218d353b96654454a19f97731af6b8ee2d64
DarkAce65 <tvdood99@gmail.com>
2016-03-17 13:44:50 -0400

    Rewrite Node to extend Point2D

--- Node.java
+++ Node.java
@@ -1,11 +1,9 @@
 import java.awt.geom.Point2D;
 
-public class Node {
-	private Node parent;
+public class Node extends Point2D.Double {
 	private int cost = 0;
-	private Point2D position;
 
 	public Node(int x, int y) {
-		position = new Point2D.Double(x, y);
+		super(x, y);
 	}
 }
