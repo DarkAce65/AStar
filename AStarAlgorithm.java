19acc56f1e61bccd2a84f1b9dd954d3837e90684
DarkAce65 <tvdood99@gmail.com>
2016-03-17 17:42:16 -0400

    Store map, start, and end in AStarAlgorithm

--- AStarAlgorithm.java
+++ AStarAlgorithm.java
@@ -1,52 +1,66 @@
 import java.util.ArrayList;
 
 public class AStarAlgorithm {
+	private Node[][] map;
+	private Node start;
+	private Node end;
+
 	private ArrayList<Node> steps;
 
-	private CostSortedNodeList<Node> open;
-	private CostSortedNodeList<Node> closed;
+	private CostSortedNodeList open;
+	private CostSortedNodeList closed;
 
 	public AStarAlgorithm() {
+		map = new Node[0][0];
+		start = null;
+		end = null;
+
 		steps = new ArrayList<Node>();
 
-		open = new CostSortedNodeList<Node>();
-		closed = new CostSortedNodeList<Node>();
+		open = new CostSortedNodeList();
+		closed = new CostSortedNodeList();
 	}
 
-	public void findPath(Node[][] map, Node start, Node end) {
-		open.empty();
-		closed.empty();
-		steps.empty();
-		start.evaluateCost(0, end);
-		open.add(start);
+	public void findPath(Node[][] map) {
+		if(evaulateMap(map)) {
+			open.clear();
+			closed.clear();
+			steps.clear();
 
-		while(open.size() > 0) {
+			// int currentCost = start.evaluateCost(0, end);
+			// open.add(start);
 
+			while(open.size() > 0) {
+
+			}
 		}
 	}
 
 	private boolean evaulateMap(Node[][] map) {
-		boolean start = false;
-		boolean end = false;
+		Node start = null;
+		Node end = null;
 		for(Node[] row : map) {
 			for(Node node : row) {
 				if(node.getType() == 1) {
-					if(start) {
-						System.out.println("More than one start positions found.");
+					if(start != null) {
+						System.out.println("More than one start position found.");
 						return false;
 					}
-					start = true;
+					start = node;
 				}
-				if(node.getType() == 2) {
-					if(end) {
-						System.out.println("More than one end positions found.");
+				else if(node.getType() == 2) {
+					if(end != null) {
+						System.out.println("More than one end position found.");
 						return false;
 					}
-					end = true;
+					end = node;
 				}
 			}
 		}
-		if(start && end) {
+		if(start != null && end != null) {
+			this.map = map;
+			this.start = start;
+			this.end = end;
 			return true;
 		}
 		System.out.println("Missing start or end position.");
