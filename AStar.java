8909b6ff8e12a76843cedee7cc610ed2e3fbebf3
DarkAce65 <tvdood99@gmail.com>
2016-03-19 15:13:11 -0400

    Simplify imports

--- AStar.java
+++ AStar.java
@@ -6,14 +6,8 @@ import java.awt.GridLayout;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.Box;
+import javax.swing.*;
 import javax.swing.border.EmptyBorder;
-import javax.swing.JButton;
-import javax.swing.JLabel;
-import javax.swing.JTextField;
-import javax.swing.JOptionPane;
 
 import java.util.ArrayList;
 
